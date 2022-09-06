c100a79c3764677d22b1ed879f2f624e4ccd7337
GodlessCrusader <44178432+GodlessCrusader@users.noreply.github.com>
2022-09-06 23:48:18 +0300

    Update ProductsCategories.sql

--- ProductsCategories.sql
+++ ProductsCategories.sql
@@ -1,2 +1,2 @@
-SELECT 'Name', 'Name' FROM dbo.Category LEFT JOIN dbo.Product JOIN dbo.ProductCategories
-ON 'CategoryId' = 'id' AND 'ProductId' = 'id'
+SELECT Product.Name, Category.Name FROM Product JOIN ProductCategories  
+ON ProductId = Product.Id  LEFT JOIN Category ON CategoryId = Category.Id
